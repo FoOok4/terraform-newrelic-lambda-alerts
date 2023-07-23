b0de6bfca713da4f5dfb3eed8faea2d5bd2d5dad
Fitim <fcakolli@blrain.com>
2023-07-24 00:09:41 +0200

    added newrelic provider

--- versions.tf
+++ versions.tf
@@ -1,3 +1,11 @@
 terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+    newrelic = {
+      source = "newrelic/newrelic"
+    }
+  }
   required_version = "~> 1.5.0"
 }
